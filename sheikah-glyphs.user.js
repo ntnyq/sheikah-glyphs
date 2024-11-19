0d314403aa207976bb1ff91c8842410b5dc9dbd3
ntnyq <ntnyq13@gmail.com>
2024-11-19 22:14:58 +0800

    chore: release v0.0.8

--- sheikah-glyphs.user.js
+++ sheikah-glyphs.user.js
@@ -3,7 +3,7 @@
 // @homepageURL  https://github.com/ntnyq/sheikah-glyphs
 // @supportURL   https://github.com/ntnyq/sheikah-glyphs
 // @namespace    sheikah-glyphs
-// @version      0.0.7
+// @version      0.0.8
 // @description  Replace all available chars to Sheikah Glyphs.
 // @author       ntnyq
 // @license      MIT
